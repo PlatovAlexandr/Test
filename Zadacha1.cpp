c008db83684f90c5804dbe3cde15ad54cf7b4fe8
PlatovAlexandr <platov080807@gmail.com>
2026-02-24 20:21:10 +0300

    Zadacha1.cpp

---
+++ Zadacha1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath> // для получения натурального логарифма (log(a) в С++ = ln(a))
+using namespace std;
+int main()
+{
+	int k, l; // k - число под степенью, l - результат степени
+	float n; //n - степень
+	cin >> k >> l;
+	n = log(l) / log(k); // logk(l) = ln(l)/ln(k) т.к. не существует конкретной формулы для логарифма
+	if ((int) n != n ) // Если целочисленний тип (int) равен дробной (float)
+	{
+		cout << "No";
+	}
+	else
+	{
+		cout << "Yes" << " " << n - 1; // n-1, т.к. в данной задаче la (степень) начинается с единицы
+	}
+	return 0;
+}
