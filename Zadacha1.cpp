0d2d485cf64ca7c9cf94a909dcf04f661a95440c
PlatovAlexandr <platov080807@gmail.com>
2026-02-28 19:17:14 +0300

    Zadacha1.cpp

--- Zadacha1.cpp
+++ Zadacha1.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-	unsigned long long n, m, a; // n - длина площади, m - ширина площади, a - сторона квадратной плитки
+	unsigned long long n, m, a, Kolvo; 
 	cin >> n >> m >> a;
-	cout << ((n + a - 1) / a) * ((m + a - 1) / a); // (n + a - 1) / a - количество плиток в длину, (m + a - 1) / a - количество плиток в ширину
+	Kolvo = ((n + a - 1) / a) * ((m + a - 1) / a);
+	cout << Kolvo;
 	return 0;
 }
