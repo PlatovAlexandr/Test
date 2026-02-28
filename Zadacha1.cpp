f87ce1f361dd2410c07fc3e6f53047b269266a8a
PlatovAlexandr <platov080807@gmail.com>
2026-02-28 18:38:25 +0300

    Revert "Zadacha1.cpp"
    This reverts commit 10c691ed00046e6af7be513cf03e276d33e43bad.

--- Zadacha1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int n, m, a; // n - длина площади, m - ширина площади, a - сторона квадратной плитки
-	cin >> n >> m >> a;
-	cout << ((n + a) / a) * ((m + a) / a); // (n + a ) / a - количество плиток в длину, (m + a) / a - количество плиток в ширину
-	return 0;
-}
